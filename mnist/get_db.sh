bfaabc06ac84b850390eb4e4344d27a19e04ae03
funnsam <funnsam8@gmail.com>
2024-12-26 04:24:47 +0000

    isolation

--- mnist/get_db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir db
-cd db
-wget https://raw.githubusercontent.com/knamdar/data/master/MNIST/raw/train-images-idx3-ubyte.gz
-wget https://raw.githubusercontent.com/knamdar/data/master/MNIST/raw/train-labels-idx1-ubyte.gz
-wget https://raw.githubusercontent.com/knamdar/data/master/MNIST/raw/t10k-images-idx3-ubyte.gz
-wget https://raw.githubusercontent.com/knamdar/data/master/MNIST/raw/t10k-labels-idx1-ubyte.gz
-gzip -d *.gz
